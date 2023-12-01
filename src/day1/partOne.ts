6847847610b38e056b85e1093e12bf4b13fdd37e
Ryszard Matula <ryszard.matula.dev@gmail.com>
2023-12-01 12:16:51 +0100

    Part 1

--- src/day1/partOne.ts
+++ src/day1/partOne.ts
@@ -1,6 +1,21 @@
-// import { readFile } from 'fs/promises'
-// import { join } from 'path'
+import { readFile } from 'fs/promises'
+import { join } from 'path'
 
 export const main = async () => {
+  const fileResult = await readFile(join(__dirname, 'input.txt'), 'utf-8')
+  let result = 0
 
+  fileResult.split('\n').forEach((line) => {
+    const numbers = line
+      .split('')
+      .filter((char) => Number(char))
+      .map(Number)
+
+    const firstNumber = numbers[0]
+    const lastNumber = numbers[numbers.length - 1] ?? numbers[0]
+
+    result += Number(`${firstNumber}${lastNumber}`)
+  })
+
+  console.log(result)
 }
